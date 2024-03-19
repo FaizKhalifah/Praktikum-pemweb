133f8bf6d97e028960b4d33c5b627852b2526947
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-03-19 08:11:19 +0700

    laprak 5

--- Praktikum4/latihan7.php
+++ Praktikum4/latihan7.php
@@ -3,11 +3,17 @@ $brush_price = 5;
 echo "<table border=\"1\" align=\"center\">";
 echo "<tr><th>Quantity</th>";
 echo "<th>Price</th></tr>";
-for ( $counter = 10; $counter <= 100; $counter += 10) {
-echo "<tr><td>";
-echo $counter;
-echo "</td><td>";
-echo $brush_price * $counter;
-echo "</td></tr>";
-}
+
+$counter = 10;
+
+
+do{
+    echo "<tr><td>";
+    echo $counter;
+    echo "</td><td>";
+    echo $brush_price * $counter;
+    echo "</td></tr>";
+    $counter += 10;
+}while($counter<=100);
+
 echo "</table>";
