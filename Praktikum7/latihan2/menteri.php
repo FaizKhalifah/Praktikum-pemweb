21cbc42c27f74889f8c8a6bf12b8a3ac21720e58
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-04-22 09:19:22 +0700

    praktikum 7 latihan 2 membuat kelas

---
+++ Praktikum7/latihan2/menteri.php
@@ -0,0 +1,14 @@
+<?php
+include ”pengurusBEM.php”;
+
+class menteri extends pengurusBEM {
+public function __construct($nama){
+$this->nama = $nama;
+}
+function setJabatan($jabatan) { // method
+$this->jabatan = $jabatan;
+}
+public function getJabatan() {
+echo $this->jabatan;
+}
+}
